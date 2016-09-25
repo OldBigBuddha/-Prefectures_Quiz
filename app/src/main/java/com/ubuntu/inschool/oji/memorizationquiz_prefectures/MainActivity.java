74144c824b3d542dc21a9ada41416f9cfcc673da
OldBigBuddha <shinlag@outlook.jp>
2016-09-25 16:58:09 +0900

    first commit

---
+++ app/src/main/java/com/ubuntu/inschool/oji/memorizationquiz_prefectures/MainActivity.java
@@ -0,0 +1,31 @@
+package com.ubuntu.inschool.oji.memorizationquiz_prefectures;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+import java.io.BufferedReader;
+
+public class MainActivity extends AppCompatActivity {
+
+    private Button startButton  = null;
+    private Intent intent       = null;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        startButton = (Button)findViewById(R.id.startButton);
+        startButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                intent = new Intent(getApplicationContext(), QuizActivity.class);
+                startActivity(intent);
+            }
+        });
+
+    }
+}
---
+++ app/src/main/java/com/ubuntu/inschool/oji/memorizationquiz_prefectures/QuizActivity.java
@@ -0,0 +1,13 @@
+package com.ubuntu.inschool.oji.memorizationquiz_prefectures;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class QuizActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_quiz);
+    }
+}
